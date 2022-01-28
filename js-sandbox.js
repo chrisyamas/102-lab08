661ca617cec37d5c7ea715c332178a6c29bff5e5
Christopher Yamas <yamas.chris@gmail.com>
2022-01-28 16:50:54 -0600

    simplified html layout, new code in js to produce images by count

--- js-sandbox.js
+++ js-sandbox.js
@@ -1,25 +1,42 @@
-function catPicCountPrompter(whichPrompt) {
-  if (!whichPrompt) {
-    whichPrompt = 'How many cat pics would you like to see?';
+alert('Welcome to my JavaScript Cat Project')
+
+function promptCatScale() {
+
+  let cScale = prompt('On a scale of 1-10, how much do you love cats?');
+  console.log('cScale', typeof Number(cScale));
+  
+  if (cScale > 7) {
+    response = 'Oh heck yeah! You\'re in the right place, my friend!';
+  } else if (cScale > 5) {
+    response = 'Alright, I don\'t love it but I respect you!';
+  } else if (cScale > 0) {
+    response = 'Um...who hurt you? You probably have an anxious attachment style.';
   }
-  let catCount = prompt(whichPrompt);
-  if (catCount === '') {
-    let message = 'How many cat pics would you like to see?';
-    let error = ' Please enter a number between 1 and 4';
-    catPicCountPrompter(message + error);
+  else {
+  let response = 'So you like to play games, eh?! Let\'s try that again...';
+  alert(response);
+  promptCatScale();
   }
-  if (catCount == 4) {
-    let counter = 0;
-    while (counter < 4) {
-      document.write('<img src="./images/cat-breadface.jpeg">')
-      counter++;
+  return alert(response);
+}
+
+promptCatScale()
+
+function catPicCountPrompter() {
+  
+  let catCount = prompt('How many cat pics you want?')
+
+  if (catCount <= 0) {
+    alert('Wow, rude. Wanna try that again, my friend?');
+    catPicCountPrompter();
+  } else if (catCount > 0) {
+    for (let i = catCount; i > 0; i--) {
+    document.write('<img src="./images/cat-keyboard.jpeg" alt="Cat Me Coding">');
     }
+  } else {
+    alert('Please enter a valid number.')
+    catPicCountPrompter();
   }
 }
 
-catPicCountPrompter();
-
-
-// let example1 = document.write('<img src=\'/images/zorkBestBoi.jpg\'>')
-
-// let example2 = document.write('<img src="/images/zorkBestBoi.jpg">')
+catPicCountPrompter()
